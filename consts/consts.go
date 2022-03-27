5d6363aba2f8c9101b51384841106c04ec825755
Andrey Mikheev <mixe.andrey@gmail.com>
2022-03-27 10:07:28 +0300

    added consts

---
+++ consts/consts.go
@@ -0,0 +1,22 @@
+package consts
+
+const (
+	DockerNameEnv                  = "DOCKER_NAME"
+	HeilaEnv                       = "HEILA_ENV"
+	DbHostEnv                      = "DB_HOST"
+	DockerName                     = "dockerName"
+	DbUsernameEnv                  = "DB_USERNAME"
+	LogToFile                      = "logToFile"
+	LogToConsole                   = "logToConsole"
+	FileJsonFormat                 = "fileJsonFormat"
+	ConsoleJsonFormat              = "consoleJsonFormat"
+	CompressLogs                   = "compressLogs"
+	SessionCollection              = "sessionCollection"
+	DisableConsoleColor            = "disableConsoleColor"
+	MinimumSeverityLevel           = "minimumSeverityLevel"
+	FxGroupLoggerContextExtractors = "loggerContextExtractors"
+	CacheStorageGetterMiddlewares  = "cacheStorageGetterMiddlewares"
+	CacheStorageSetterMiddlewares  = "cacheStorageSetterMiddlewares"
+	MonitorTags                    = "monitorTags"
+	DiscoveryServiceProvider       = "discoveryServiceProvider"
+)
