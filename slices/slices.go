42e828b63788fcb77a78ee3ea1c9257b14fc8609
Andrey <andrey.m@heilasystems.com>
2022-04-13 11:39:01 +0000

    Merged in POS-5387-dbupgrade-digital-support (pull request #6)
    added from helpers
    
    * added from helpers
    
    * Merge branch 'dev' into POS-5387-dbupgrade-digital-support
    
    # Conflicts:
    #	slices/slices.go
    
    
    Approved-by: Guy Brandwine

--- slices/slices.go
+++ slices/slices.go
@@ -10,3 +10,41 @@ func (u UniqueIdSlice) Add(newId string) UniqueIdSlice {
 	}
 	return append(u, newId)
 }
+
+func ArrayFromMap(m map[string]interface{}) []interface{} {
+	var arr []interface{}
+	for k, v := range m {
+		arr = append(arr, k)
+		arr = append(arr, v)
+	}
+	if len(arr) > 0 {
+		return arr
+	}
+	return nil
+}
+
+func ArrayStringFromMap(m map[string]string) []string {
+	var arr []string
+	for k, v := range m {
+		arr = append(arr, k)
+		arr = append(arr, v)
+	}
+	if len(arr) > 0 {
+		return arr
+	}
+	return nil
+}
+
+func GetStringSliceIntersection(s1, s2 []string) (inter []string) {
+	hash := make(map[string]bool)
+	for _, e := range s1 {
+		hash[e] = true
+	}
+	for _, e := range s2 {
+		if hash[e] {
+			inter = append(inter, e)
+			hash[e] = false
+		}
+	}
+	return
+}
