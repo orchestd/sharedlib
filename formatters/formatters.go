c6670d1517f2f5d3da41abf3755c0d7474d7487e
Natan Donder <Natan.d@heilasystems.com>
2023-12-13 13:17:40 +0200

    POS-8632: rollback

--- formatters/formatters.go
+++ formatters/formatters.go
@@ -1,20 +1,10 @@
 package formatters
 
 import (
-	"fmt"
 	"math"
-	"strings"
 )
 
-func countDecimalLen(price float64) int {
-	return len(strings.Split(fmt.Sprint(price), ".")[1])
-}
-
 func Round(price float64, decimals int) float64 {
-	decimalsAmount := countDecimalLen(price)
-	if decimalsAmount < decimals {
-		decimals = decimalsAmount
-	}
 	precision := math.Pow10(decimals)
 	return math.Round(price*precision) / precision
 }
